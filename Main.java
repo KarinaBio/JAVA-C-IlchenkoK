2f967695034a2fde4ba80da669864cc6569e4934
KarinaBio <55648901+KarinaBio@users.noreply.github.com>
2021-12-07 13:25:22 +0200

    Ilchenko hw 5

--- Main.java
+++ Main.java
@@ -2,33 +2,52 @@ package com.univ;
 
 import java.util.Scanner;
 
-
 public class Main {
-    
-    // HW 4 Task 4.20.k
-    public static void main(String[] args) {
-        int deg = 2;
-        int i = 1, ii = 3, j = 8, jj = 6, k = -1;
-        double x, ans = 1, tmp;
-        Scanner scanner = new Scanner(System.in);
-        x = scanner.nextDouble();
-        ans += x / 2.0;
-        while (true) {
-            tmp = Math.pow(-1, k) * (Math.pow(x, deg) * i) / j;
-            if (Math.abs(tmp) > 0.001) {
-                ans += tmp;
-                ii += 2;
-                i *= ii;
-                jj += 2;
-                j *= jj;
-                deg += 1;
-                k += 1;
 
-                continue;
+    public static void main(String[] args) {
+        Task_1_14();
+        Task_1_26();
+    }
+
+    public static void Task_1_14() {
+        System.out.println("Введіть рядок чисел: ");
+        Scanner scanner = new Scanner(System.in);
+        String[] array = scanner.nextLine().split(" ");
+        Integer[] mas = new Integer[array.length];
+        for (int i = 0; i < mas.length; i++) {
+            mas[i] = Integer.parseInt(array[i]);
+        }
+        Integer[] mas_ans = new Integer[array.length];
+        mas_ans[0] = mas[0];
+        int j = 0;
+        for (int i = 1; i < mas.length; i++) {
+            if (mas[i] != mas_ans[j]){
+                j += 1;
+                mas_ans[j] = mas[i];
             }
-            System.out.println(ans);
-            System.out.println(Math.sqrt(1.0+x));
-            break;
+        }
+        for (int i = 0; i <= j; i++) {
+            System.out.println(mas_ans[i]);
+        }
+    }
+    public static void Task_1_26() {
+        System.out.println("Введіть кількість пенні в монетах: ");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        // [50 , 25 , 10, 1]
+        Integer[] mas = new Integer[4];
+        String[] mas_str = {"50 коп", "25 коп","10 коп","1 коп"};
+        mas[0] = n / 50;
+        n = n % 50;
+        mas[1] = n / 25;
+        n = n % 25;
+        mas[2] = n / 10;
+        n = n % 10;
+        mas[3] = n;
+        for (int i = 0; i < mas.length; i++) {
+            if (mas[i] != 0){
+                System.out.println(mas_str[i] + " * " + mas[i]);
+            }
         }
     }
 }
